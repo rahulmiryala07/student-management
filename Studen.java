83b0fb8779129df7b191b4fde2bce996939ca9fb
rahulmiryala07 <151643892+rahulmiryala07@users.noreply.github.com>
2025-11-30 23:06:10 +0530

    Update Studen.java

---
+++ Studen.java
@@ -0,0 +1,8 @@
+package hello;
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello!");
+    System.out.println("Hello!");
+  }
+}
